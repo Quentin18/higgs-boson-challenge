6668dcc17b23a0b331507ce080c2e6ec0bb5ffa9
Quentin18 <quentindeschamps18@orange.fr>
2021-10-21 15:58:51 +0200

    update scripts

---
+++ scripts/predict_logistic_regression.py
@@ -0,0 +1,60 @@
+# flake8: noqa: E402
+import os
+import sys
+import zipfile
+import numpy as np
+
+# Add src to path to import implementations
+sys.path.append('../src')
+
+# Import functions
+from proj1_helpers import (create_csv_submission, load_csv_data, predict_labels,
+                           standardize)
+from implementations import logistic_regression_GD
+
+DATA_DIRECTORY = '../data'
+DATA_TRAIN_PATH = os.path.join(DATA_DIRECTORY, 'train.csv')
+DATA_TEST_PATH = os.path.join(DATA_DIRECTORY, 'test.csv')
+
+OUTPUT_DIRECTORY = '../out'
+OUTPUT_PATH = os.path.join(OUTPUT_DIRECTORY, 'predictions.csv')
+
+# Extract archives if needed
+for csv_filename in (DATA_TRAIN_PATH, DATA_TEST_PATH):
+    if not os.path.exists(csv_filename):
+        zip_filename = csv_filename + '.zip'
+        with zipfile.ZipFile(zip_filename, 'r') as zf:
+            zf.extractall(DATA_DIRECTORY)
+
+# Create output directory if needed
+if not os.path.exists(OUTPUT_DIRECTORY):
+    os.mkdir(OUTPUT_DIRECTORY)
+
+# Load the train data
+print('1/5 Load train data')
+y, tX, ids = load_csv_data(DATA_TRAIN_PATH)
+# Standardize data
+tX = standardize(tX)
+
+# Logistic regression
+print('2/5 Run logistic regression')
+initial_w = np.zeros((tX.shape[1], 1))
+max_iters = 10000
+gamma = 0.01
+threshold = 1e-8
+w, loss = logistic_regression_GD(y, tX, initial_w, max_iters, gamma, threshold)
+
+# Load test data
+print('3/5 Load test data')
+_, tX_test, ids_test = load_csv_data(DATA_TEST_PATH)
+tX_test = standardize(tX_test)
+
+# Generate predictions
+print('4/5 Generate predictions')
+y_pred = predict_labels(w, tX_test)
+
+# Create submission
+print('5/5 Create submission')
+create_csv_submission(ids_test, y_pred, OUTPUT_PATH)
+
+print(f'File {OUTPUT_PATH} created')
--- scripts/proj1_helpers.py
+++ scripts/proj1_helpers.py
@@ -50,3 +50,8 @@ def create_csv_submission(ids, y_pred, name):
         writer.writeheader()
         for r1, r2 in zip(ids, y_pred):
             writer.writerow({'Id': int(r1), 'Prediction': int(r2)})
+
+
+def standardize(x):
+    """Outputs the matrix x after normalization."""
+    return (x - np.mean(x, axis=0)) / np.std(x, axis=0)
