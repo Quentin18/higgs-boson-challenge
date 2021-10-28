0b8cdcce64223a3706882ea873f0e07647bc2c98
eseiler <emilien.seiler@epfl.ch>
2021-10-28 12:50:05 +0200

    anova test

--- src/stats_tests.py
+++ src/stats_tests.py
@@ -5,25 +5,20 @@ import numpy as np
 from split_data import split_by_label
 
 
-def anova_test(x : np.ndarray, y : np.ndarray, label_b=-1)-> list:
-    """
-        Anova test on features to see which one are relevant.
-        
-        Args:
-        x (np.ndarray) : input data (data * features)
-        y (np.ndarray) : input labels of x
-        label_b (int) : label of b (0 or -1(default))
+def anova_test(y: np.ndarray, x: np.ndarray, label_b: int = -1) -> tuple:
+    """Anova test on features for features selection
 
-        Output:
-        list of index of irrelevant features.   
-    
+    Args:
+        y (np.ndarray): input data features
+        x (np.ndarray): input data
+        label_b (int, optional):  default value -1.
+
+    Returns:
+        tuple : (ind_F,F) F f_value of features sorted, ind_F index of features
     """
-    tX_a,tX_b=split_by_label(y, x, label_b)
+    tX_a, tX_b = split_by_label(y, x, label_b)
     mean_labels = [np.mean(tX_a, 0), np.mean(tX_b, 0)]
-    # Between the sum of square (variance between the mean of each groups)
-    ssb = np.var(mean_labels, 0)
-    # Within the sum of square (variance between)
-    ssw = np.var(tX_a, 0) + np.var(tX_b, 0)
-    f = ssb / ssw
-    print(np.argsort(f))
-    return f
+    var_between_groups = np.var(mean_labels, 0)
+    var_in_groups = np.var(tX_a, 0) + np.var(tX_b, 0)
+    f = var_between_groups / var_in_groups
+    return np.argsort(f), np.sort(f)
