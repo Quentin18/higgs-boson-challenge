ba2e930c5ad29932fed5353077970748d3830dbc
Quentin18 <quentindeschamps18@orange.fr>
2021-10-29 01:32:07 +0200

    update main_predict_by_jet

--- scripts/main_predict_by_jet.py
+++ scripts/main_predict_by_jet.py
@@ -6,123 +6,111 @@ import os
 import numpy as np
 
 # Import functions from scripts/
-from path import (add_src_to_path, create_out_dir, extract_archives,
-                  DATA_TEST_PATH, DATA_TRAIN_PATH, OUT_DIR)
-from proj1_helpers import create_csv_submission, load_csv_data
+from csv_utils import create_csv_submission, load_csv_data
+from path import (add_src_to_path, load_json_parameters, DATA_TEST_PATH,
+                  DATA_TRAIN_PATH, OUT_DIR)
 
-# Add src/ to path to import implementations
+# Add src/ to path to import functions
 add_src_to_path()
 
 # Import functions from src/
-from clean_data import clean_data_by_jet
-from implementations import (least_squares, logistic_regression,
-                             reg_logistic_regression)
+from clean_data import clean_data_by_jet, get_mean_std, standardize
+from cross_validation import build_poly
+from implementations import ridge_regression
 from split_data import split_by_jet
 from helpers import predict_labels
-from metrics import get_proportions
-from print_utils import print_shapes, print_shapes_by_jet
+from print_utils import (print_shapes, print_shapes_by_jet, print_subset_label,
+                         NB_SUBSETS)
 
 # Classifier to use
-# CLASSIFIER = 'least_squares'
-# CLASSIFIER = 'logistic_regression'
-CLASSIFIER = 'reg_logistic_regression'
+CLASSIFIER = 'ridge_regression'
 
 # Output file path
-OUTPUT_PATH = os.path.join(OUT_DIR, 'predictions_test.csv')
+OUTPUT_PATH = os.path.join(OUT_DIR, 'predictions_ridge_regression.csv')
 
 
 def main():
     """
     Main function to generate predictions for AIcrowd.
     """
-    # Extract archives if needed
-    extract_archives()
-
-    # Create output directory if needed
-    create_out_dir()
-
     # Load the train data
-    print('[1/7] Load train data')
-    y, x, ids = load_csv_data(DATA_TRAIN_PATH, label_b=0)
-    print_shapes(y, x)
+    print('[1/8] Load train data')
+    y_tr, x_tr, ids_tr = load_csv_data(DATA_TRAIN_PATH)
+    print_shapes(y_tr, x_tr)
 
     # Load the test data
-    print('[2/7] Load test data')
-    y_test, x_test, ids_test = load_csv_data(DATA_TEST_PATH, label_b=0)
-    print_shapes(y_test, x_test)
+    print('[2/8] Load test data')
+    y_te, x_te, ids_te = load_csv_data(DATA_TEST_PATH)
+    print_shapes(y_te, x_te)
 
     # Split and clean train data
-    print('[3/7] Split and clean train data')
-    y_by_jet, x_by_jet, _ = split_by_jet(y, x, ids)
-    cols_to_remove_by_jet = clean_data_by_jet(y_by_jet, x_by_jet)
-    print_shapes_by_jet(y_by_jet, x_by_jet)
+    print('[3/8] Split and clean train data')
+    y_tr_by_jet, x_tr_by_jet, _ = split_by_jet(y_tr, x_tr, ids_tr)
+    cols_to_remove_by_jet = clean_data_by_jet(y_tr_by_jet, x_tr_by_jet,
+                                              std=False)
+    print_shapes_by_jet(y_tr_by_jet, x_tr_by_jet)
 
     # Split and clean test data
-    print('[4/7] Split and clean test data')
-    y_test_by_jet, x_test_by_jet, ids_test_by_jet = split_by_jet(
-        y_test, x_test, ids_test)
-    clean_data_by_jet(y_test_by_jet, x_test_by_jet, cols_to_remove_by_jet)
-    print_shapes_by_jet(y_test_by_jet, x_test_by_jet)
+    print('[4/8] Split and clean test data')
+    y_te_by_jet, x_te_by_jet, ids_te_by_jet = split_by_jet(y_te, x_te, ids_te)
+    clean_data_by_jet(y_te_by_jet, x_te_by_jet, cols_to_remove_by_jet,
+                      std=False)
+    print_shapes_by_jet(y_te_by_jet, x_te_by_jet)
 
-    print('[5/7] Run classification algorithm')
+    # Standardize
+    for i in range(NB_SUBSETS):
+        mean, std = get_mean_std(x_tr_by_jet[i])
+        x_tr_by_jet[i] = standardize(x_tr_by_jet[i])
+        x_te_by_jet[i] = standardize(x_te_by_jet[i], mean, std)
 
+    # Load parameters
+    print('[5/7] Load parameters')
+    params = load_json_parameters()
+    print(params[CLASSIFIER])
+    lambda_ = params[CLASSIFIER]['lambda_']
+    degrees = params[CLASSIFIER]['degree']
+
+    # Run ridge regression
+    print('[6/8] Run classification algorithm')
     w_by_jet = list()
-    if CLASSIFIER == 'least_squares':
-        for i, x_tr_jet, y_tr_jet in zip(range(len(x_by_jet)),
-                                         x_by_jet, y_by_jet):
-            print(f'Jet = {i}:')
-            w, _ = least_squares(y_tr_jet, x_tr_jet)
-            w_by_jet.append(w)
 
-    elif CLASSIFIER == 'logistic_regression':
-        max_iters = 5000
-        gamma = 1e-5
-        threshold = 1e-8
-        for i, x_tr_jet, y_tr_jet in zip(range(len(x_by_jet)),
-                                         x_by_jet, y_by_jet):
-            print(f'Jet = {i}:')
-            initial_w = np.zeros((x_tr_jet.shape[1], 1))
-            w, _ = logistic_regression(y_tr_jet, x_tr_jet, initial_w,
-                                       max_iters, gamma, threshold,
-                                       info=True, agd=True)
-            w_by_jet.append(w)
+    for i in range(NB_SUBSETS):
+        print_subset_label(i)
 
-    elif CLASSIFIER == 'reg_logistic_regression':
-        lambda_ = 1e-3
-        max_iters = 5000
-        gamma = 1e-5
-        threshold = 1e-8
-        for i, x_tr_jet, y_tr_jet in zip(range(len(x_by_jet)),
-                                         x_by_jet, y_by_jet):
-            print(f'Jet = {i}:')
-            initial_w = np.zeros((x_tr_jet.shape[1], 1))
-            w, _ = reg_logistic_regression(y_tr_jet, x_tr_jet, lambda_,
-                                           initial_w, max_iters, gamma,
-                                           threshold, info=True)
-            w_by_jet.append(w)
+        # Get train subset
+        x_tr_jet, y_tr_jet = x_tr_by_jet[i], y_tr_by_jet[i]
 
-    else:
-        print('Unknown classifier')
-        return
+        # Build polynomial basis
+        phi_tr_jet = build_poly(x_tr_jet, degrees[i])
+
+        # Run ridge regression
+        w, loss = ridge_regression(y_tr_jet, phi_tr_jet, lambda_)
+        print(f'Loss = {loss:.3f}')
+
+        # Add weights to list
+        w_by_jet.append(w)
 
     # Generate predictions
-    print('[6/7] Generate predictions')
+    print('[7/8] Generate predictions')
+
     y_pred_by_jet = list()
-    for i, w, x_te_jet, y_te_jet in zip(range(len(x_by_jet)), w_by_jet,
-                                        x_test_by_jet, y_test_by_jet):
-        x_te_jet = np.c_[np.ones((y_te_jet.shape[0], 1)), x_te_jet]
-        y_pred = predict_labels(w, x_te_jet, label_b_in=0, label_b_out=-1,
-                                use_sigmoid=True)
+
+    for i in range(NB_SUBSETS):
+        # Get subset
+        x_te_jet, w = x_te_by_jet[i], w_by_jet[i]
+
+        # Build polynomial basis
+        phi_te_jet = build_poly(x_te_jet, degrees[i])
+
+        # Predict labels
+        y_pred = predict_labels(w, phi_te_jet)
         y_pred_by_jet.append(y_pred)
 
     # Create submission
-    print('[7/7] Create submission')
+    print('[8/8] Create submission')
 
     y_pred = np.concatenate(y_pred_by_jet)
-    ids_pred = np.concatenate(ids_test_by_jet)
-
-    proportions = get_proportions(y_pred)
-    print('Proportions:', proportions)
+    ids_pred = np.concatenate(ids_te_by_jet)
 
     create_csv_submission(ids_pred, y_pred, OUTPUT_PATH)
 
