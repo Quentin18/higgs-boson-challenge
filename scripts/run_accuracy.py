fix gradient descent

scripts/run_accuracy.py
@@ -112,7 +112,10 @@ def main():
         # Get train subset
         x_tr_jet, y_tr_jet = x_tr_by_jet[i], y_tr_by_jet[i]
 
-        if is_logistic or is_gradient:
+        if is_gradient:
+            clf_params['initial_w'] = np.zeros(x_tr_jet.shape[1])
+
+        elif is_logistic:
             clf_params['initial_w'] = np.zeros((x_tr_jet.shape[1], 1))
 
         if lambdas:

src/implementations.py
@@ -82,7 +82,7 @@ def least_squares_SGD(y: np.ndarray, tx: np.ndarray, initial_w: np.ndarray,
     t_start = time.time()
 
     w = initial_w
-    for _ in range(max_iters):
+    for iter in range(max_iters):
         for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size):
             # Compute gradient
             grad = least_squares_gradient(minibatch_y, minibatch_tx, w)